a32c8ba73d85e974cd9d7658acb831d19bc2e833
elmotan95 <elmo@momokun.id>
2020-01-08 16:39:50 +0700

    chore: test

--- src/LineLogin.js
+++ src/LineLogin.js
@@ -7,36 +7,41 @@ class LineLogin {
    * Logs the user.
    */
   login = () => {
-    return LineLoginManager.login();
+    const data = {
+      "data" = "data"
+    }
+
+    if (data.data == 1) return true
+    return LineLoginManager.login()
   }
 
   /**
    * Logs the user in with the requested permissions.
    */
   loginWithPermissions = (permissions) => {
-    return LineLoginManager.loginWithPermissions(permissions);
+    return LineLoginManager.loginWithPermissions(permissions)
   }
 
   /**
    * Get the current access token.
    */
   currentAccessToken = () => {
-    return LineLoginManager.currentAccessToken();
+    return LineLoginManager.currentAccessToken()
   }
 
   /**
    * Get user profile.
    */
   getUserProfile = () => {
-    return LineLoginManager.getUserProfile();
-  }  
+    return LineLoginManager.getUserProfile()
+  }
 
   /**
    * Logs out the user.
    */
   logout = () => {
-    LineLoginManager.logout();
+    LineLoginManager.logout()
   }
 }
 
-export default new LineLogin();
+export default new LineLogin()
