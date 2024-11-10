f9c4bc319230a4ab9589eb0163840e690f04bab7
Arda Satata <ardasatata@gmail.com>
2024-11-11 02:38:20 +0900

    add ios implementation

--- plugins/withLineSDK.js
+++ plugins/withLineSDK.js
@@ -1,12 +1,4 @@
-const {
-  withDangerousMod,
-  withPlugins,
-  IOSConfig,
-  withInfoPlist,
-} = require('@expo/config-plugins')
-const { resolve } = require('path')
-const { readFileSync, writeFileSync, promises } = require('fs')
-const { withLineLogin } = require('./ios/withLinePluginConfig')
+const { withPlugins } = require('@expo/config-plugins')
 
 // Android
 const withApplyLineImplementation = require('./android/withApplyLineImplementation')
@@ -16,112 +8,9 @@ const withApplyAndroidCompileOptions = require('./android/withApplyAndroidCompil
 const withLineChannelId = require('./android/withLineChannelId')
 
 // iOS
-const lineCFBundleURLSchemes = {
-  CFBundleURLSchemes: ['line3rdp.$(PRODUCT_BUNDLE_IDENTIFIER)'],
-}
-
-const lineLSApplicationQueriesSchemes = 'lineauth2'
-
-const CHANNEL_ID_REPLACE = 'CHANNEL_ID_REPLACE'
-
-// iOS mods
-// Pass `<string>` to specify that this plugin requires a string property.
-function withCustomPlist(config, { channelId }) {
-  return withInfoPlist(config, config => {
-    config.modResults.CFBundleURLTypes?.push(lineCFBundleURLSchemes)
-
-    let LSScheme = config.modResults.LSApplicationQueriesSchemes
-
-    if (!LSScheme) {
-      config.modResults.LSApplicationQueriesSchemes = [
-        lineLSApplicationQueriesSchemes,
-      ]
-    } else {
-      if (
-        config.modResults.LSApplicationQueriesSchemes.find(
-          value => value === lineLSApplicationQueriesSchemes,
-        )
-      ) {
-        // Do nothing
-      } else {
-        config.modResults.LSApplicationQueriesSchemes = [
-          lineLSApplicationQueriesSchemes,
-        ]
-      }
-    }
-
-    config.modResults.LineSDKConfig = {
-      ChannelID: channelId,
-    }
-
-    return config
-  })
-}
-
-function withForcedAppDelegate(config, { channelId }) {
-  return withDangerousMod(config, [
-    'ios',
-    async config => {
-      const fileInfo = IOSConfig.Paths.getAppDelegate(
-        config.modRequest.projectRoot,
-      )
-
-      // const envProfile = config.extra.env.RELEASE_STAGE ?? 'development'
-
-      let { path } = fileInfo
-
-      const { projectRoot } = config.modRequest
-
-      // V1
-      // const newAppDelegateFile = resolve(projectRoot, 'plugins/line-sdk/ios/', envProfile , 'appDelegate.m');
-      // V2
-      const newAppDelegateFile = resolve(
-        projectRoot,
-        'plugins/line-sdk/ios/',
-        'appDelegate_template.m',
-      )
-
-      // V1
-      const newAppDelegate = readFileSync(newAppDelegateFile, 'utf8')
-
-      // V2
-      const replacedAppDelegate = newAppDelegate.replace(
-        CHANNEL_ID_REPLACE,
-        channelId,
-      )
-
-      const oldAppDelegateFile = resolve(path)
-
-      await promises.writeFile(oldAppDelegateFile, replacedAppDelegate)
-
-      return config
-    },
-  ])
-}
-
-function withLinePod(config) {
-  return withDangerousMod(config, [
-    'ios',
-    cfg => {
-      const { platformProjectRoot } = cfg.modRequest
-      const podfile = resolve(platformProjectRoot, 'Podfile')
-      const contents = readFileSync(podfile, 'utf-8')
-      const lines = contents.split('\n')
-      const index = lines.findIndex(line => /\s+use_expo_modules!/.test(line))
-
-      writeFileSync(
-        podfile,
-        [
-          ...lines.slice(0, index),
-          `  pod 'LineSDKSwift', '~> 5.0'`,
-          ...lines.slice(index),
-        ].join('\n'),
-      )
-
-      return cfg
-    },
-  ])
-}
+const withCustomPlist = require('./ios/withCustomPlist')
+const withLinePod = require('./ios/withLinePod')
+const withAppDelegateMod = require('./ios/withAppDelegateMod')
 
 function withLineSDK(config, props) {
   if (!props?.channelId) {
@@ -131,8 +20,7 @@ function withLineSDK(config, props) {
     // iOS
     withLinePod,
     [withCustomPlist, props],
-    [withLineLogin, props],
-    // [withForcedAppDelegate, props],
+    [withAppDelegateMod, props],
 
     // Android
     withApplyLineImplementation,
