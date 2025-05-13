e0562f8cf343248925749f1967a3a01571aef012
Joaquin Aguirre <aguirre.miguez@outlook.com>
2025-05-13 16:49:44 -0300

    chore: add support for Expo 53 and the swift version of the AppDelegate

--- plugin/ios/withCustomAppDelegate.js
+++ plugin/ios/withCustomAppDelegate.js
@@ -2,24 +2,51 @@ const { withAppDelegate } = require('@expo/config-plugins')
 
 const generateCode = require('@expo/config-plugins/build/utils/generateCode')
 
+function selectStrings(language) {
+  if (language === 'swift') {
+    return {
+      import: {
+        anchor: /import\s+Expo/,
+        newSrc: 'import react_native_line',
+      },
+      return: {
+        anchor: /return\s+super\.application\(\s*app,\s*open:\s*url,\s*options:\s*options\s*\)\s*\|\|\s*RCTLinkingManager\.application\(\s*app,\s*open:\s*url,\s*options:\s*options\s*\)/,
+        newSrc: '    return LineLogin.application(app, open: url, options: options)',
+        offset: 0,
+      },
+    }
+  } else {
+    return {
+      import: {
+        anchor: /#import\s+"AppDelegate\.h"/,
+        newSrc: '#import "react_native_line-Swift.h"',
+      },
+      return: {
+        anchor: /-\s*\(BOOL\)\s*application:\s*\(UIApplication\s*\*\)\s*application\s+openURL:\s*\(NSURL\s*\*\)\s*url\s+options:\s*\(NSDictionary<UIApplicationOpenURLOptionsKey,\s*id>\s*\*\)\s*options\s*\{/,
+        newSrc: '    return [LineLogin application:application open:url options:options];',
+      },
+    }
+  }
+}
+
 function withCustomAppDelegate(config) {
   return withAppDelegate(config, config => {
+    const strings = selectStrings(config.modResults.language)
+
     const withImport = generateCode.mergeContents({
       comment: '//',
-      anchor: /#import "AppDelegate\.h"/,
-      newSrc: '#import "react_native_line-Swift.h"',
       offset: 1,
       src: config.modResults.contents,
       tag: 'import',
+      ...strings.import,
     })
 
     const withOpenUrl = generateCode.mergeContents({
       comment: '//',
-      anchor: /- \(BOOL\)application:\(UIApplication \*\)application openURL:\(NSURL \*\)url options:\(NSDictionary<UIApplicationOpenURLOptionsKey,id> \*\)options {/,
-      newSrc: 'return [LineLogin application:application open:url options:options];',
       offset: 1,
       src: withImport.contents,
-      tag: 'return',
+      tag: 'linking',
+      ...strings.return,
     })
 
     return {
